76b3196b53fd19e0d527922ab98beaad90bde6f5
Jack Gamboa <jgamboa929@gmail.com>
2019-03-25 16:44:41 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import "./App.css";
 
 class App extends Component {
+  state = {
+    textLength: null
+  };
+
+  onTextChangeHandler = event => {
+    let textValue = event.target.value;
+    // console.log(textValue.length);
+
+    const stateCopy = { ...this.state };
+    stateCopy.textLength = textValue.length;
+
+    this.setState(stateCopy);
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h2>Enter some text</h2>
+        <input type="text" onChange={this.onTextChangeHandler} />
+        <p>{this.state.textLength}</p>
       </div>
     );
   }
